8b8d778f29db2d5c0ea41dab54f5772b3564220a
Trang <Trang@family-HP.wireless-1x.unc.edu>
2019-09-01 17:41:18 -0400

    Fixed some of the for loops.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,26 +5,25 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
-		// Read in count of numbers to process
+
+		// Read in count of customers to process
 		// count: the total number of customers
 		int count = scan.nextInt();
-		
-		// Read in customer information
-		// itemAmt: number of items bought by the customer
-		String firstName = scan.next();
-		String lastName = scan.next();
-		int itemAmt = scan.nextInt();
-		double total = 0;
-		
+
 		// Make a loop so the scanner gets information from
 		// every customer
 		for (int i = 0; i < count; i++) {
-		
+			// Read in customer information
+			// itemAmt: number of items bought by the customer
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int itemAmt = scan.nextInt();
+			double total = 0;
+
 			// Run for loop so that the scanner will get all the
 			// information for EACH item
 			for (int j = 0; j < itemAmt; j++) {
@@ -34,12 +33,13 @@ public class A1Novice {
 				double price = scan.nextDouble();
 				total += (quant * price);
 			}
+			System.out.println(firstName.charAt(0) + ". " + lastName + ": " + total);
 		}
-		System.out.println(firstName.charAt(0) + ". " + lastName + ": " + total);
-		
+
+
 		scan.close();
-		
-			
+
+
 	}
-								
+
 }
