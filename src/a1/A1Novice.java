2707498feb1ee5ab439bf31c1b67e2340c2f6279
Trang <Trang@family-HP.wireless-1x.unc.edu>
2019-09-01 17:19:34 -0400

    Submitting first try

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,36 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Read in count of numbers to process
+		// count: the total number of customers
+		int count = scan.nextInt();
+		
+		// Read in customer information
+		// itemAmt: number of items bought by the customer
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int itemAmt = scan.nextInt();
+		double total = 0;
+		
+		// Make a loop so the scanner gets information from
+		// every customer
+		for (int i = 0; i < count; i++) {
+		
+			// Run for loop so that the scanner will get all the
+			// information for EACH item
+			for (int j = 0; j < itemAmt; j++) {
+				// quant: quantity of the item bought
+				// Read in item information for one item
+				int quant =  scan.nextInt();;
+				double price = scan.nextDouble();
+				total += (quant * price);
+			}
+		}
+		System.out.println(firstName.charAt(0) + ". " + lastName + ": " + total);
+		
+		scan.close();
+		
+			
 	}
+								
 }
